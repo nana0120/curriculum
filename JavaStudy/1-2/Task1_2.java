feb4310342accede124e9063ffc36aaf58f427b4
nana0120 <nana.asakuma1125@gmail.com>
2022-09-13 22:24:21 +0900

    課題提出
    配列の作成

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,15 @@
+public class Task1_2{
+    public static void main (String[] args){
+        String[] country = {"JAPAN","AMERICA","KOREA"};
+        System.out.println(country.length);
+        String[] strArray = new String[3];
+        strArray[0]="りんご";
+        strArray[1]="もも";
+        strArray[2]="ぶどう";
+        System.out.println(strArray[1]);
+        // int[]型の変数intArrayの宣言と初期化
+        int[] intArray = {10,20,30,40,50};
+        //intArrayの2番目と5番目の数を足し合わせて表示
+        System.out.println(intArray[1]+intArray[4]);
+    }
+}
