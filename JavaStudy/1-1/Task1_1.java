8a45e7db45c0d030a1f7a3117d2de38e074f4fa9
nana0120 <nana.asakuma1125@gmail.com>
2022-09-13 21:14:38 +0900

    課題提出
    データ型・文字列クラスの作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,15 @@
+public class Task1_1{
+
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        // double型のhogeDoubleという名前の変数で宣言し、値「0.2」初期化
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        // 変数hogeStringの値を表示
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+    }
+}
