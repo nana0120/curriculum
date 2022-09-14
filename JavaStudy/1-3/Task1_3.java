c1304a46435fbdbf0d20cdf1fc95d56e70a2bb27
nana0120 <nana.asakuma1125@gmail.com>
2022-09-14 20:35:11 +0900

    課題提出
    演算子の作成

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,18 @@
+public class Task1_3{
+    public static void main(String[]args){
+        int plus = 5 + 10;
+        int minus = 20 - 7;
+        // 10と2を乗算した値を代入する、変数multiplyを作成
+        int multiply = 10 * 2;
+        int remi = 20 %6;
+        // 文字列変数 townの有楽町と文字列変数 lineの線
+        String town ="有楽町";
+        String line ="線";
+        String train = town + line;
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+    }
+}
