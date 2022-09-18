9332fff4dbefa457e28d94a4c463b163a1ac850d
nana0120 <nana.asakuma1125@gmail.com>
2022-09-19 01:26:15 +0900

    課題提出
    パッセージの作成

---
+++ JavaStudy/2-4/src/study/Study.java
@@ -0,0 +1,10 @@
+package study;
+import constants.FruitsConstants;
+import fruits.Fruits;
+
+public class Study {
+	public static void main(String[] args) {
+		Fruits.printFruits(FruitsConstants .FRUITS_LEMON_04,FruitsConstants .FRUITS_COUNT_20);
+	}
+
+}
